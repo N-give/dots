a7e23dfe2fb754c568f2bd2e9c5a5a821753ed54
nate <nate.givens03@gmail.com>
2019-05-02 08:03:07 -0400

    first thing I noticed needed to be computer specific - the screen locking mechanism

--- .config/i3/setup_config.sh
+++ .config/i3/setup_config.sh
@@ -4,4 +4,6 @@ cat ~/.config/i3/config.base > ~/.config/i3/config
 
 if [ "$HOSTNAME" == "gon" ]; then
   cat ~/.config/i3/config.gon >> ~/.config/i3/config
+else
+  cat ~/.config/i3/config.saitama >> ~/.config/i3/config
 fi
