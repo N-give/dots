feac00789c656d56be76ab99a95581fdf5f6399c
nate <nate.givens03@gmail.com>
2019-05-02 08:09:38 -0400

    first try didn't work, but this did for now

--- .config/i3/setup_config.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-cat ~/.config/i3/config.base > ~/.config/i3/config
-
-if [ "$HOSTNAME" == "gon" ]; then
-  cat ~/.config/i3/config.gon >> ~/.config/i3/config
-else
-  cat ~/.config/i3/config.saitama >> ~/.config/i3/config
-fi
