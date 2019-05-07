9954e7b089ec5f563b4ffa6b38e1e50764abe524
nate <nate.givens03@gmail.com>
2019-05-07 18:04:26 -0400

    fixing startup script. maybe not the best way to do it

---
+++ .config/i3/i3_setup.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+cat ~/.config/i3/config.base > ~/.config/i3/config
+
+if [ "$HOSTNAME" == "gon" ]; then
+  cat ~/.config/i3/config.gon >> ~/.config/i3/config
+  feh --bg-scale ~/Pictures/debian_wallpaper.png
+elif [ "$HOSTNAME" == "saitama" ]; then
+  cat ~/.config/i3/config.saitama >> ~/.config/i3/config
+  feh --bg-scale ~/Pictures/debian_wallpaper.png
+else
+  cat ~/.config/i3/config.other >> ~/.config/i3/config
+  feh --bg-scale ~/Pictures/rand_pic.png
+fi
+
+
