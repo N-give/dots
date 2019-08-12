aca468af5d711d325fb77c46e049dac202164bb7
Nathan Givens <nate.givens03@gmail.com>
2019-08-12 10:24:30 -0400

    fixing screen layout for new monitor

--- .config/i3/i3_setup.sh
+++ .config/i3/i3_setup.sh
@@ -9,6 +9,10 @@ elif [ "$HOSTNAME" == "saitama" ]; then
   cat ~/.config/i3/config.saitama >> ~/.config/i3/config
   xrandr --output HDMI-0 --left-of DP-0
   feh --bg-fill ~/Pictures/debian_wallpaper.png
+elif [ "$HOSTNAME" == "ngivens-OptiPlex-7020" ]; then
+  xrandr --output DVI-0 --left-of DisplayPort-0
+  cat ~/.config/i3/config.other >> ~/.config/i3/config
+  feh --bg-scale ~/Pictures/solarized-wallpaper-ubuntu.png
 else
   cat ~/.config/i3/config.other >> ~/.config/i3/config
   feh --bg-scale ~/Pictures/rand_pic.png
